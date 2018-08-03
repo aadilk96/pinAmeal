49773d678eb76358442bbdcf288af24b1734677a
Aadil Anil Kumar <aadilanilkumar@gmail.com>
2018-08-03 20:18:13 +0400

    respage+

---
+++ pinAmeal/src/pages/respage/respage.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { RespagePage } from './respage';
+
+@NgModule({
+  declarations: [
+    RespagePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(RespagePage),
+  ],
+})
+export class RespagePageModule {}
