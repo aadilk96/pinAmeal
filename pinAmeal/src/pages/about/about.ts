c0dcf6f63f4b4c72d60fc7fee5a9a699f889bff9
Aadil Anil Kumar <aadilanilkumar@gmail.com>
2018-08-03 17:21:40 +0400

    restaurants list+

--- pinAmeal/src/pages/about/about.ts
+++ pinAmeal/src/pages/about/about.ts
@@ -6,18 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  items: any[];
   constructor(public navCtrl: NavController) {
-    this.items =[];
-    for(let i = 1; i < 11; i++){
-      this.items.push({
-        text: "Restaurant " + i,
-        id: i
-      });
-    }
+  }
 
-  }
-  itemSelected(item){
-    alert(item.text);
-  }
 }
